ee64d2fcae25a0191b1b709b0b8b7bf2b215c588
cristian <cregueiromartinez@danielcastelao.org>
2024-11-14 19:54:36 +0100

    haciendo clase hilo

--- src/Ascensor.java
+++ src/Ascensor.java
@@ -1,4 +1,36 @@
 public class Ascensor {
 
+    private boolean parado = true;
+    Piso pisoObject = new Piso();
+
+    public synchronized void subir(int piso, int idPersona) throws InterruptedException {
+        if(!parado){
+            wait();
+        }
+        if(piso > pisoObject.getNumPiso()){
+            parado = false;
+            System.out.println("Persona numero " + idPersona + " ha pulsado el boton " + piso);
+            System.out.println("Subiendo...");
+            System.out.println("Piso: " + piso + "/20");
+            parado = true;
+            notifyAll();
+        }
+
+    }
+
+    public synchronized void bajar(int piso, int idPersona) throws InterruptedException {
+        if(!parado){
+            wait();
+        }
+        if(piso < pisoObject.getNumPiso()){
+            parado = false;
+            System.out.println("Persona numero " + idPersona + " ha pulsado el boton " + piso);
+            System.out.println("bajando...");
+            System.out.println("Piso: " + piso + "/20");
+            parado = true;
+            notifyAll();
+        }
+    }
+
 
 }
--- src/Hilo.java
+++ src/Hilo.java
@@ -1,2 +1,22 @@
-public class Hilo {
+import java.util.Random;
+
+public class Hilo implements Runnable{
+
+    private int idPiso;
+    private Ascensor ascensor;
+    private Random rand;
+
+    public Hilo(int idPiso) {
+        this.idPiso = idPiso;
+    }
+
+
+    @Override
+    public void run() {
+        try{
+            ascensor.subir(rand.nextInt(1)+10, idPiso);
+        } catch (InterruptedException e) {
+            System.out.println("Ups, ha ocurrido un error en la ejecucion");
+        }
+    }
 }
